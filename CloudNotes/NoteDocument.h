ce8959917fe4247b0dc7cc0a32a6f898a5eb957a
Tom Harrington <tph@atomicbird.com>
2011-10-30 16:37:12 -0600

    Add NoteDocument, based on WWDC 2011 session 116 video

---
+++ CloudNotes/NoteDocument.h
@@ -0,0 +1,21 @@
+//
+//  NoteDocument.h
+//  CloudNotes
+//
+//  Created by Tom Harrington on 10/30/11.
+//  Copyright (c) 2011 Atomic Bird, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NoteDocument;
+
+@protocol NoteDocumentDeletate <NSObject>
+- (void)noteDocumentContentsUpdated:(NoteDocument *)document;
+@end
+
+@interface NoteDocument : UIDocument
+
+@property (strong, readonly) NSString *documentText;
+@property (weak, readwrite) id delegate;
+@end
---
+++ CloudNotes/NoteDocument.m
@@ -0,0 +1,42 @@
+//
+//  NoteDocument.m
+//  CloudNotes
+//
+//  Created by Tom Harrington on 10/30/11.
+//  Copyright (c) 2011 Atomic Bird, LLC. All rights reserved.
+//
+
+#import "NoteDocument.h"
+
+@interface NoteDocument ()
+@property (strong, readwrite) NSString *documentText;
+@end
+
+@implementation NoteDocument
+@synthesize documentText = __documentText;
+@synthesize delegate = __delegate;
+
+- (BOOL)loadFromContents:(id)contents ofType:(NSString *)typeName error:(NSError *__autoreleasing *)outError
+{
+	NSString *text = nil;
+	if ([contents length] > 0) {
+		text = [[NSString alloc] initWithBytes:[contents bytes] length:[contents length] encoding:NSUTF8StringEncoding];
+	} else {
+		text = @"";
+	}
+	[self setDocumentText:text];
+	
+	if ([[self delegate] respondsToSelector:@selector(noteDocumentContentsUpdated:)]) {
+		[[self delegate] noteDocumentContentsUpdated:self];
+	}
+	return YES;
+}
+
+- (id)contentsForType:(NSString *)typeName error:(NSError *__autoreleasing *)outError
+{
+	if ([[self documentText] length] == 0) {
+		[self setDocumentText:@"New note"];
+	}
+	return [NSData dataWithBytes:[[self documentText] UTF8String] length:[[self documentText] length]];
+}
+@end
